1924aaa6c3d9af24c1f0d43407c223fd1484b2ea
Alexandre Mathurin <35438645+ToxicBowl@users.noreply.github.com>
2024-05-14 06:40:43 -0400

    Update DataContainer.java
    I also started attempting to implement the mapping for champions to their respective IDs, but its unfinished.

--- Match_Data_Container/bin/DataContainer.java
+++ Match_Data_Container/bin/DataContainer.java
@@ -4,11 +4,12 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
 
 public class DataContainer {
     // Lists to store data extracted from CSV
     private List<Integer> matchID;
-    private List<Integer> matchDuration;
     private List<String> matchOutcome;
     private List<Integer> kills;
     private List<Integer> deaths;
@@ -17,13 +18,13 @@ public class DataContainer {
     private List<Integer> damageDealt;
     private List<Integer> damageTaken;
     private List<Integer> wardsPlaced;
+    private List<Integer> playerChampionID;
     private List<String> playerChampion;
     private List<String> playerRole;
 
     // Constructor to initialize lists
     public DataContainer() {
         matchID = new ArrayList<>();
-        matchDuration = new ArrayList<>();
         matchOutcome = new ArrayList<>();
         kills = new ArrayList<>();
         deaths = new ArrayList<>();
@@ -32,82 +33,42 @@ public class DataContainer {
         damageDealt = new ArrayList<>();
         damageTaken = new ArrayList<>();
         wardsPlaced = new ArrayList<>();
+        playerChampionID = new ArrayList<>();
         playerChampion = new ArrayList<>();
         playerRole = new ArrayList<>();
     }
 
     // Method to read data from CSV file
-    public void readDataFromCSV(String filePaths, int matchIdIndex, int matchDurationIndex, int matchOutcomeIndex,
-            int killsIndex, int deathsIndex, int assistsIndex, int goldEarnedIndex, int damageDealtIndex,
-            int damageTakenIndex, int wardsPlacedIndex, int playerChampionIndex, int playerRoleIndex, List<Integer> specificLines) {
-    	
-    	List<String> fileList;
-        if (filePaths instanceof String) {
-            fileList = new ArrayList<>();
-            fileList.add((String) filePaths);
-        } else if (filePaths instanceof List<?>) {
-            fileList = (List<String>) filePaths;
-        } else {
-            throw new IllegalArgumentException("Invalid argument type for filePaths");
-        }
-        
-    for (String filePath : filePaths) {  
-    	try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+    public void readDataFromCSV(String filePath) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             boolean headerSkipped = false;
-            int lineNumber = 0;
-            
             while ((line = br.readLine()) != null) {
-            	lineNumber++;
                 if (!headerSkipped) {
                     headerSkipped = true;
                     continue; // Skip header row
                 }
-                
-                
-             // Check if the current line number is in the list of specific lines
-                if (!specificLines.contains(lineNumber)) {
-                    continue; // Skip this line if not in the list
-                }
-                
                 String[] data = line.split(",");
-               
-                // Parse data and add to lists
-                matchID.add(Integer.parseInt(data[matchIdIndex]));
-                matchDuration.add(Integer.parseInt(data[matchDurationIndex]));
-                matchOutcome.add(data[matchOutcomeIndex].equals("0") ? "Defeat" : "Victory");;
-                kills.add(Integer.parseInt(data[killsIndex]));
-                deaths.add(Integer.parseInt(data[deathsIndex]));
-                assists.add(Integer.parseInt(data[assistsIndex]));
-                goldEarned.add(Integer.parseInt(data[goldEarnedIndex]));
-                damageDealt.add(Integer.parseInt(data[damageDealtIndex]));
-                damageTaken.add(Integer.parseInt(data[damageTakenIndex]));
-                wardsPlaced.add(Integer.parseInt(data[wardsPlacedIndex]));
-                playerChampion.add(data[playerChampionIndex]);
-                playerRole.add(data[playerRoleIndex]);
+                
+             
+                    matchID.add(Integer.parseInt(data[0]));
+                    matchOutcome.add(data[1].equals("0") ? "Defeat" : "Victory");
+                    kills.add(Integer.parseInt(data[2]));
+                    deaths.add(Integer.parseInt(data[3]));
+                    assists.add(Integer.parseInt(data[4]));
+                    damageDealt.add(Integer.parseInt(data[5]));
+                    damageTaken.add(Integer.parseInt(data[6]));
+                    goldEarned.add(Integer.parseInt(data[7]));
+                    wardsPlaced.add(Integer.parseInt(data[8]));
+                    playerRole.add(data[9]);
+                    playerChampionID.add(data[10]);
+                 
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (NumberFormatException e) {
-            // Handle the case where an invalid number format is encountered
+        } catch (IOException | NumberFormatException e) {
             e.printStackTrace();
         }
-        
     }
-  }
     
-    
-    /** Method to read data from multiple CSV files for specific columns
-    public void readDataFromCSV(List<String> filePaths, int matchIdIndex, int matchDurationIndex,
-            int matchOutcomeIndex, int killsIndex, int deathsIndex, int assistsIndex, int goldEarnedIndex,
-            int damageDealtIndex, int damageTakenIndex, int wardsPlacedIndex, int playerChampionIndex,
-            int playerRoleIndex) {
-        for (String filePath : filePaths) {
-            readDataFromCSV(filePath, matchIdIndex, matchDurationIndex, matchOutcomeIndex, killsIndex, deathsIndex,
-                    assistsIndex, goldEarnedIndex, damageDealtIndex, damageTakenIndex, wardsPlacedIndex,
-                    playerChampionIndex, playerRoleIndex);
-        }
-    } **/
 
     // Method to perform bootstrapping
     public List<DataContainer> bootStrap(int numSamples) {
@@ -117,7 +78,6 @@ public class DataContainer {
      // Determine the smallest size among all lists
         int minSize = Integer.MAX_VALUE;
         minSize = Math.min(minSize, matchID.size());
-        minSize = Math.min(minSize, matchDuration.size());
         minSize = Math.min(minSize, matchOutcome.size());
         minSize = Math.min(minSize, kills.size());
         minSize = Math.min(minSize, deaths.size());
@@ -126,7 +86,7 @@ public class DataContainer {
         minSize = Math.min(minSize, damageDealt.size());
         minSize = Math.min(minSize, damageTaken.size());
         minSize = Math.min(minSize, wardsPlaced.size());
-        minSize = Math.min(minSize, playerChampion.size());
+        minSize = Math.min(minSize, playerChampionID.size());
         minSize = Math.min(minSize, playerRole.size());
         
         // Perform bootstrapping for the specified number of samples
@@ -138,7 +98,6 @@ public class DataContainer {
                 int randomIndex = random.nextInt(numData());
                 // Add data corresponding to the randomly selected index to the bootstrap sample
                 bootstrapSample.matchID.add(matchID.get(randomIndex));
-                bootstrapSample.matchDuration.add(matchDuration.get(randomIndex));
                 bootstrapSample.matchOutcome.add(matchOutcome.get(randomIndex));
                 bootstrapSample.kills.add(kills.get(randomIndex));
                 bootstrapSample.deaths.add(deaths.get(randomIndex));
@@ -147,7 +106,7 @@ public class DataContainer {
                 bootstrapSample.damageDealt.add(damageDealt.get(randomIndex));
                 bootstrapSample.damageTaken.add(damageTaken.get(randomIndex));
                 bootstrapSample.wardsPlaced.add(wardsPlaced.get(randomIndex));
-                bootstrapSample.playerChampion.add(playerChampion.get(randomIndex));
+                bootstrapSample.playerChampionID.add(playerChampionID.get(randomIndex));
                 bootstrapSample.playerRole.add(playerRole.get(randomIndex));
             }
 
@@ -159,12 +118,18 @@ public class DataContainer {
     }
 
     // Method to get the number of data points
-    private int numData() {
+    public int numData() {
         // Assuming all lists have the same size
         return matchID.size();
     }
     
-    
+ // Method to match champion IDs to names
+    public void matchChampionNames(Map<Integer, String> championMap) {
+        for (Integer championId : playerChampionID) {
+            String championName = championMap.get(championId);
+            playerChampion.add(championName);
+        }
+    }
     
  
     // Method to fill missing data
@@ -175,15 +140,12 @@ public class DataContainer {
     
     
     // Getters for data lists
-    // Add setters if needed (I dont think we need to though)
+    // Add setters if needed 
 
     public List<Integer> getMatchID() {
         return matchID;
     }
 
-    public List<Integer> getMatchDuration() {
-        return matchDuration;
-    }
 
     public List<String> getMatchOutcome() {
         return matchOutcome;
@@ -217,7 +179,7 @@ public class DataContainer {
         return wardsPlaced;
     }
 
-    public List<String> getPlayerChampion() {
+    public List<String> getplayerChampion() {
         return playerChampion;
     }
 
